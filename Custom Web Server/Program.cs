f1fd472f15b0776ca85685bb8e55037b59ea1715
Joro <joro_paspalev@abv.bg>
2020-09-17 11:07:17 +0300

    Add Web server Project

---
+++ Custom Web Server/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Custom_Web_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener tcpListener = new TcpListener(IPAddress.Loopback, 1212);
+            tcpListener.Start();
+
+            while (true)
+            {
+                var client = tcpListener.AcceptTcpClient();
+                using (var stream = client.GetStream())
+                {
+                    byte[] buffer = new byte[100000];
+                    int length = stream.Read(buffer, 0, buffer.Length);
+
+                    
+                    string requestString = Encoding.UTF8.GetString(buffer, 0, length);
+
+                    Console.WriteLine(requestString);
+
+                    Console.WriteLine(new string('-', 70));
+
+                    string html = "<h1> Joro is the best programmer </h1>";
+
+                    string response = "HTTP/1.1 200 OK" + "\r\n"
+                        + "Content-Type: text/html" + "\r\n"
+                        + "Content-Lenght:" + html.Length + "\r\n"
+                        + "\r\n"
+                        + html + "\r\n";
+
+                    byte[] responseAsByteArray = Encoding.UTF8.GetBytes(response);
+                    stream.Write(responseAsByteArray);
+                }
+            }
+
+
+
+
+
+        }
+    }
+}
---
+++ HttpClient/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpClient1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+          
+
+
+
+            await ReadData();
+
+        }
+
+        public static async Task ReadData()
+        {
+            string url = "https://www.abv.bg";
+
+            HttpClient httpClient = new HttpClient();
+
+            HttpResponseMessage response = await httpClient.GetAsync(url);
+
+            string data = "key=value";
+
+            HttpContent httpContent = new StringContent(data);
+
+            HttpResponseMessage responseMessage = await httpClient.PostAsync(url, httpContent);
+
+
+
+
+            ;
+
+        }
+
+
+
+
+
+
+    }
+}
